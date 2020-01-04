185c63e45897731da75264e4541035637c55de3d
Michal <michal.rozanski.94@gmail.com>
2020-01-04 23:45:17 +0100

    Create first draft of MagazineApp

--- MagazineApp/MainWindow.xaml.cs
+++ MagazineApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Rest;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,29 +21,70 @@ namespace MagazineApp
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public const string SERVICE_URL = "https://localhost:44315/";
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        public static MagazineWebService2 GetBlogWebClient(string uri = SERVICE_URL)
+        {
+            var client = new MagazineWebService2(new Uri(uri), new BasicAuthenticationCredentials());
+            return client;
+        }
+
         private void ButtonAdd_Click(object sender, RoutedEventArgs e)
         {
+            var client = GetBlogWebClient();
 
+            string name = NameValue.Text;
+            int count = Int32.Parse(CountValue.Text);
+            string localization = LocalizationValue.Text;
+
+            client.Magazine.AddMagazineProduct(name, count, localization);
+            RefreshListOfEntires();
         }
 
         private void ButtonDelete_Click(object sender, RoutedEventArgs e)
         {
+            var client = GetBlogWebClient();
 
+            int id = Int32.Parse(idValue.Text);
+            int count = Int32.Parse(CountValue.Text);
+            string localization = LocalizationValue.Text;
+
+            client.Magazine.RemoveMagazineProduct(id);
+            RefreshListOfEntires();
         }
 
         private void ButtonModify_Click(object sender, RoutedEventArgs e)
         {
+            var client = GetBlogWebClient();
 
+            int id = Int32.Parse(idValue.Text);
+            int count = Int32.Parse(CountValue.Text);
+
+            client.Magazine.ModifyMagazineProduct(id, count);
+            RefreshListOfEntires();
         }
 
+        private void RefreshListOfEntires()
+        {
+            var client = GetBlogWebClient();
+            var list = client.Magazine.GetAllProducts();
+
+            ListOfEntires.Items.Clear();
+
+            foreach (var item in list)
+            {
+                ListOfEntires.Items.Add(item);
+            }
+        }
         private void ButtonRefresh_Click(object sender, RoutedEventArgs e)
         {
-
+            RefreshListOfEntires();
         }
     }
 }
